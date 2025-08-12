bf9382ffd288e7ba0a703899d7eec0d7c39f540b
Mykola <nikolay.moroz2006@gmail.com>
2025-08-12 23:56:11 +0300

    add Maui project to my console project

---
+++ SWAPI/SwapiMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SwapiMaui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SWAPI/SwapiMaui/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SwapiMaui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SwapiMaui.Pages")]
