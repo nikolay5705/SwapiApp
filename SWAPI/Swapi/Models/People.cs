47361cc9f49cf33ecad1e79227b8a3fcfe15199c
Mykola <nikolay.moroz2006@gmail.com>
2025-08-12 22:23:15 +0300

    second commit about maui

--- SWAPI/Swapi/Models/People.cs
+++
@@ -1,9 +0,0 @@
-namespace SWAPI.SwapiModels
-{
-    public class People
-    {
-        public string Name { get; set; } = "";
-        public string Gender { get; set; } = "";
-        public string Birth_Year { get; set; } = "";
-    }
-}
